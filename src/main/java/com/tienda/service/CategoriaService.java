16a0c0eef93d75581e405f71a17142b575c514c7
Brandonns576 <brandonnso576@gmail.com>
2025-02-26 23:44:34 -0600

    semana 5 finalizacion de CRUD Categoria

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -19,4 +19,23 @@ public class CategoriaService {
         var lista = categoriaRepository.findAll();
    return lista;
     }
+
+    //se escriben los metodos de un CRUD Create Read Update Delete
+    @Transactional(readOnly=true)
+    public Categoria getCategoria(Categoria categoria){
+       
+   return categoriaRepository.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+   @Transactional
+    public void delete(Categoria categoria){
+       categoriaRepository.delete(categoria);
+    }
+    
+    @Transactional
+    public void save(Categoria categoria){
+       //si categoria.idCategoria esta vacio se incerta registro
+       //si categoria tiene algo se modifica ese registro
+        categoriaRepository.save(categoria);
+    }
 }
